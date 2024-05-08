89784f6daf264a6ec06ca51fddddc42230c59996
Crispurdycl <cristianandres.cardenas1@alumnos.ulagos.cl>
2024-05-08 06:52:51 -0400

    añadimos imagenes con descripción

--- TPA_RESERVA_HORAS.py
+++ TPA_RESERVA_HORAS.py
@@ -1,6 +1,11 @@
 import tkinter as tk            # para la interfaz gráfica
 import json                     # para guardar las reservas en un archivo .json para simular una base de datos
 from tkinter import messagebox  # para mostrar mensajes de error o información
+from PIL import Image, ImageTk  # para cargar imágenes en la interfaz gráfica
+
+
+
+
 
 # Clase para realizar reservas
 # Se pueden realizar reservas en una cancha de fútbol, tenis o pádel
@@ -18,8 +23,8 @@ class Reservas():
 
         # Creación de la ventana principal, definición de su tamaño y título
 
-        self.ventana = tk.Tk() 
-        self.ventana.geometry('800x600')
+        self.ventana = tk.Tk()
+        self.ventana.geometry('1600x1200')
         self.ventana.title("Reservaciones ICINF")
         self.ventana.configure(bg='#f0f0f0')
 
@@ -82,7 +87,38 @@ class Reservas():
         self.abrir_reserva_boton = tk.Button(self.frame_principal, text="Ver reservas", command=self.abrir_reserva, font=self.estilo_boton, bg=self.color_boton, fg=self.color_texto_boton, padx=10, pady=5, bd=0)
         self.abrir_reserva_boton.pack(pady=10)
 
-    # Métodos de la clase
+        # creamos un diccionario con las imagenes de los recintos para que no se borren al ejecutar el programa
+
+        self.imagenes = {
+
+            "Cancha de Fútbol": Image.open("cancha.png"),
+            "Cancha de Tenis": Image.open("tenis.png"),
+            "Cancha de Pádel": Image.open("padel.png")
+        
+        }
+
+        self.imagen_label = tk.Label(self.frame_principal, bg='#f0f0f0')
+        self.imagen_label.pack()
+
+        # Creamos un diccionario con las descripciones de los recintos
+
+        self.descripcion =  {
+
+            'Cancha de Fútbol': 'Panamá, Osorno, Chile. Cancha de fútbol de césped sintético, baños y vestuarios disponibles, horario de 8:00 hasta 23:00.\n Entrada accesible para personas en silla de ruedas.\n Estacionamiento accesible para personas en silla de ruedas. \n Piscina disponible. \n Ideal para ir con niños.',
+            'Cancha de Tenis': 'José Fruto Sáez S/N, Osorno, Chile. Cancha de tenis de arcilla, baños y vestuarios disponibles, horario de 8:00 hasta 23:00.',
+            'Cancha de Pádel': 'Camino a Puerto Octay km 1 , Osorno, Chile. Cancha de pádel de cemento, baños y vestuarios disponibles, horario de 8:00 hasta 23:00.'
+
+        }
+
+        self.descripcion_label = tk.Label(self.frame_principal, bg='#f0f0f0')
+        self.descripcion_label.pack()
+
+
+    ###############################################################################
+    ########################### MÉTODOS DE LA CLASE ###############################
+    ###############################################################################
+
+
     # Llamamos a la función reservar() cuando se presiona el botón "Reservar"
     # Esta función se encarga de validar los datos ingresados por el usuario y de realizar la reserva
     # Si los datos son correctos, se guarda la reserva en un archivo .json y se muestra un mensaje de éxito
@@ -90,11 +126,10 @@ class Reservas():
 
     def reservar(self): # Método
 
-        nombre = str(self.usuario_entry.get()) # Se obtienen los datos ingresados por el usuario
+        nombre = str(self.usuario_entry.get()).lower() # Se obtienen los datos ingresados por el usuario
         recinto = self.recinto_select.get() # Se obtienen los datos ingresados por el usuario x2
         hora = self.hora_select.get() # Se obtienen los datos ingresados por el usuario x3
-        nombre.lower() # Se convierte el nombre de usuario a minúsculas
-
+    
         while True: # Bucle infinito para validar los datos ingresados por el usuario
 
             if not nombre: # Si no se ingresa un nombre de usuario
@@ -142,6 +177,8 @@ class Reservas():
                 self.cancelar() # Se llama a la función cancelar() para limpiar los campos de usuario, recinto y hora
                 break
 
+
+
     # Método para cargar las horas disponibles según el recinto seleccionado
     # Se cargan las horas disponibles según el recinto seleccionado
     # Si el recinto es una cancha de fútbol, tenis o pádel, se cargan las horas de 08:00 a 23:00
@@ -152,16 +189,31 @@ class Reservas():
         recinto = self.recinto_select.get() # Se obtiene el recinto seleccionado
         horas = [] # Se crea una lista para almacenar las horas disponibles
 
-        if recinto in ["Cancha de Fútbol", "Cancha de Tenis", "Cancha de Pádel"]: # Si el recinto es una cancha de fútbol, tenis o pádel
+        if recinto in ["Cancha de Fútbol", "Cancha de Tenis", "Cancha de Pádel"]: # Si el recinto es una cancha de fútbol, tenis o pádel: # Si el recinto es una cancha de fútbol, tenis o pádel
 
-            horas = ["Seleccione hora", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"] # Se cargan las horas de 08:00 a 23:00
+            horas = ["Seleccione hora", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
+                        
+            self.mostrar_imagen(recinto) # Se llama a la función mostrar_imagen() para mostrar la imagen del recinto seleccionado
 
-        self.hora_select.set(horas[0]) # Se selecciona la primera hora disponible
-        self.hora_select_menu['menu'].delete(0, 'end') # Se eliminan las horas disponibles del menú desplegable
+        if recinto == "Seleccione una opción": # Si no se selecciona un recinto
 
-        for hora in horas: # Se recorren las horas disponibles
+            self.hora_select.set("Seleccione hora")
+            self.hora_select_menu['menu'].delete(0, 'end')
+            self.imagen_label.config(image=None) # Se muestra la imagen del recinto seleccionado
+            self.descripcion_label.config(text="")
+            self.imagen_label.image = None
+            
+        self.hora_select.set(horas[0]) # Se establece la primera hora como la hora seleccionada
+        self.hora_select_menu['menu'].delete(0, 'end')
+        
+        for hora in horas:
+        
+            self.hora_select_menu['menu'].add_command(label=hora, command=tk._setit(self.hora_select, hora))
 
-            self.hora_select_menu['menu'].add_command(label=hora, command=tk._setit(self.hora_select, hora)) # Se cargan las horas disponibles en el menú desplegable
+        mostrar_descripcion = self.descripcion.get(recinto) # Se obtiene la descripción del recinto seleccionado
+        self.descripcion_label.config(text=mostrar_descripcion) # Se muestra la descripción del recinto seleccionado
+
+
 
     # Método para cancelar la reserva
     # Se limpian los campos de usuario, recinto y hora
@@ -171,6 +223,9 @@ class Reservas():
         self.usuario_entry.delete(0, tk.END) # Se limpian los campos de usuario, recinto y hora
         self.recinto_select.set("Seleccione una opción") # Se limpian los campos de usuario, recinto y hora
         self.hora_select.set("Seleccione hora") # Se limpian los campos de usuario, recinto y hora
+        self.imagen_label.image = None
+
+
 
     # Método para guardar las reservas en un archivo .json
     # Se guardan las reservas en un archivo .json para simular una base de datos
@@ -180,6 +235,9 @@ class Reservas():
         with open("reservas.json", "w") as fecha1: # Se abre el archivo .json de reservas
 
             json.dump(self.reservas_guardadas, fecha1) # Se guardan las reservas en el archivo .json
+        # la letra "w" indica que se va a escribir en el archivo .json y si el archivo no existe, se crea uno nuevo
+        # por otro lado la letra "r" indica que se va a leer el archivo .json y si el archivo no existe, se produce un error
+
 
     # Método para listar las reservas guardadas
     # Se listan las reservas guardadas en el archivo .json
@@ -192,9 +250,11 @@ class Reservas():
 
         for reserva in self.reservas_guardadas: # Se recorren las reservas guardadas
 
-            self.reservas_text.insert(tk.END, f"Nombre: {reserva['nombre']}\nRecinto: {reserva['recinto']}\nHora: {reserva['hora']}\n\n") # Se muestra la reserva en el cuadro de texto de reservas
+            self.reservas_text.insert(tk.END, f" {self.imagen} \n Nombre: {reserva['nombre']}\nRecinto: {reserva['recinto']}\nHora: {reserva['hora']}\n\n") # Se muestra la reserva en el cuadro de texto de reservas
         
         self.reservas_text.config(state=tk.DISABLED) # Se deshabilita la edición del cuadro de texto de reservas
+
+
 
     # Método para abrir la ventana de visualización de reservas
     # Se cierra la ventana actual y se abre la ventana de visualización de reservas
@@ -204,6 +264,36 @@ class Reservas():
         self.ventana.destroy() # Se cierra la ventana actual
         app2 = VisualizarReservas() # Se crea una instancia de la clase VisualizarReservas
         app2.ventana.mainloop() # Se inicia la ventana principal
+
+
+
+    # Método para mostrar la imagen del recinto seleccionado
+    # Se muestra la imagen del recinto seleccionado en la interfaz gráfica
+    # Este metodo es llamado por la funcion cargar_horas()
+    # cuando esta se ejecuta y se selecciona un recinto
+
+    def mostrar_imagen(self, recinto): # Método
+
+        imagen = self.imagenes.get(recinto) # Se obtiene la imagen del recinto seleccionado
+        imagen = imagen.resize((400, 400), Image.ANTIALIAS) # Se redimensiona la imagen del recinto seleccionado
+        
+        # antialias es un método de suavizado de la imagen para evitar el efecto de escalera
+        # se utiliza para mejorar la calidad de la imagen redimensionada
+        # para entender mejor el efecto de escalera, se puede ver en la imagen de la cancha de fútbol como las líneas de la cancha se ven pixeladas
+
+        if imagen: # Si se encuentra la imagen del recinto seleccionado
+
+            imagen = ImageTk.PhotoImage(imagen) # Se carga la imagen del recinto seleccionado
+            self.imagen_label.config(image=imagen) # Se muestra la imagen del recinto seleccionado
+            self.imagen_label.image = imagen  # Referencia necesaria para evitar que la imagen sea eliminada por el recolector de basura
+        
+        else: # Si no se encuentra la imagen del recinto seleccionado
+        
+            self.imagen_label.image = None # Se muestra la imagen del recinto seleccionado, o en este caso, no se muestra ninguna imagen
+
+
+
+
 
 # clase para visualizar las reservas guardadas
 # Se muestra un cuadro de texto con las reservas guardadas
@@ -279,7 +369,9 @@ class VisualizarReservas():
         self.cargar_reservas() # Se llama a la función cargar_reservas() para cargar las reservas guardadas en el archivo .json
         self.esta_vacia() # Se llama a la función esta_vacia()
 
-    # Métodos de la clase
+    ###############################################################################
+    ########################### MÉTODOS DE LA CLASE ###############################
+    ###############################################################################
 
     # Llamamos a la función esta_vacia() cuando se abre la ventana
     # Esta función verifica si hay reservas guardadas
@@ -299,6 +391,8 @@ class VisualizarReservas():
 
             self.cargar_reservas() # Se cargan las reservas guardadas en el cuadro de texto de reservas
 
+
+
     # Método para cargar las reservas guardadas
     # Se cargan las reservas guardadas en el archivo .json
     # Se cargan las reservas guardadas en el cuadro de texto de reservas
@@ -316,6 +410,8 @@ class VisualizarReservas():
                 self.reservas_text.insert(tk.END, f"Nombre: {reserva['nombre']}\nRecinto: {reserva['recinto']}\nHora: {reserva['hora']}\n\n") # Se muestra la reserva en el cuadro de texto de reservas
             
             self.reservas_text.config(state=tk.DISABLED) # Se deshabilita la edición del cuadro de texto de reservas
+
+
 
     # Método para buscar una reserva por el nombre del usuario
     # Se busca una reserva por el nombre del usuario
@@ -345,6 +441,8 @@ class VisualizarReservas():
         if not encontradas: # Si no se encuentra ninguna reserva
 
             messagebox.showinfo("Información", "No se encontraron reservas con ese nombre.") # Se muestra un mensaje de información
+
+
 
     # Método para eliminar una reserva por el nombre del usuario
     # Se elimina una reserva por el nombre del usuario
@@ -393,6 +491,8 @@ class VisualizarReservas():
             
         messagebox.showinfo("Información", "No existe una reserva con ese nombre") # Se muestra un mensaje de información
 
+
+
     # Método para guardar las reservas en un archivo .json
     # Se guardan las reservas en un archivo .json para simular una base de datos
 
@@ -402,6 +502,8 @@ class VisualizarReservas():
             
             json.dump(self.reservas, fecha1) # Se guardan las reservas en el archivo .json
 
+
+
     # Método para volver a la ventana de reservas
     # Se cierra la ventana actual y se abre la ventana de reservas
 
@@ -410,6 +512,10 @@ class VisualizarReservas():
         self.ventana.destroy() # Se cierra la ventana actual
         app = Reservas() # Se crea una instancia de la clase Reservas
         app.ventana.mainloop() # Se inicia la ventana principal
+
+
+
+
 
 # Función principal
 # Se crea una instancia de la clase Reservas
